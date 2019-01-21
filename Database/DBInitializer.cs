4a84b7241834a151552cae5bee1f68e40964cf41
MAYA\Maya <Mahlateyared@gmail.com>
2019-01-21 15:59:09 -0800

    Admin  and landing pages

---
+++ Database/DBInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Domain;
+namespace Database
+{
+    public class DBInitialize: DropCreateDatabaseIfModelChanges<BiranaDatabase>
+    {
+        protected override void Seed(BiranaDatabase context)
+        {
+            // add stuffs here
+            base.Seed(context);
+        }
+    }
+}
